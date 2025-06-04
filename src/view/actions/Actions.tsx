8f49e29faf9a9712526160780e151cb384ff1896
serjG901 <70537823+serjG901@users.noreply.github.com>
2025-06-05 01:10:41 +0300

    Update Actions.tsx

--- src/view/actions/Actions.tsx
+++ src/view/actions/Actions.tsx
@@ -113,7 +113,7 @@ export default function Actions({ useActionsStore }: ActionsComponent) {
                 badge: "./images/android/android-launchericon-96-96.png",
                 body: `in action - ${action.activity.name}`,
                 icon: "./images/android/android-launchericon-192-192.png",
-                tag: "action",
+                tag: `${action.activity.name}`,
               });
               self.addEventListener("notificationclick", (event) => {
                 //@ts-expect-error notif
